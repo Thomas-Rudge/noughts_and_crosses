fd9db93114781413e6d30a377a4fb79c908b978e
Thomas-Rudge <thomas.rudge85@gmail.com>
2016-11-23 16:16:00 +0000

    Building the Board class
    Ive fixed a few problems with the Game class, and have starting work on the Board class. It works for 2 player mode, but still needs work.

--- noughts_and_crosses.rb
+++ noughts_and_crosses.rb
@@ -1,76 +1,83 @@
-WINNING_GAMES = [[7, 8, 9], [4, 5, 6], [1, 2, 3], # Horizontal
-                 [1, 4, 7], [2, 5, 8], [3, 6, 9], # Vertical
-                 [1, 5, 9], [3, 5, 7]]            # Diagonal
+WINNING_GAMES = [[7, 8, 9], [4, 5, 6], [1, 2, 3], # Horizontal _7_|_8_|_9_
+                 [1, 4, 7], [2, 5, 8], [3, 6, 9], # Vertical   _4_|_5_|_6_
+                 [1, 5, 9], [3, 5, 7]]            # Diagonal    1 | 2 | 3
 
 class Game
-  private :get_players, :get_char_pref, :get_starter, :set_players
-  attribute_reader :scores
-  attribute_accessor :options
-  
   def initialize
-    @affirmative = ['y', 'yes', 'yep', 'sure', 'ok', 'okay']
+    @affirmative = ["y", "yes", "yep", "sure", "ok", "okay"]
     @scores      = Hash.new
     @options     = {mode: 0, turn: 0, charset: [:X, :O]}
   end
-  
+
   def start
     puts "Would you like to play a game?"
     print "\t>>"
-    response = gets.chomp.downcase.gsub(' ', '')
-    
+    response = gets.chomp.downcase.gsub(" ", "")
+
     if @affirmative.include? response
-      setup()
-      board = Board.new(self)
-      board.play
+      setup
+      go_to_board
     else
-      finish()
+      finish
     end
   end
-  
+
+  def go_to_board
+    board = Board.new(self)
+    winner = board.play
+
+    @scores[winner] += 1
+
+    get_scoreboard
+
+    replay?
+  end
+
   def replay?
     puts "Would you like to play again?"
     print ">>"
-    response = get.chomp.downcase.gsub(' ', '')
-    
-    @affirmative.include? response
+    response = gets.chomp.downcase.gsub(" ", "")
+
+    (@affirmative.include? response) ? go_to_board : finish
   end
-  
+
   def setup
-    get_players()
-    set_players()
-    get_char_pref()
-    get_starter()
-    
+    get_players
+    set_players
+    get_starter
   end
-  
+
   def get_scoreboard
-    max_name_length = @scores.keys.max { |a, b| a.length <=> b.length }.length + 1
-    
-    puts "\n#{' ' * max_name_length} : Score"
-    
+    max_name_length = @scores.keys.max { |a, b| a.length <=> b.length }.length
+
+    puts "\n#{" " * max_name_length} : Score"
+
     @scores.each do |player, score|
-      puts "#{player.ljust(max_name_length)} : #{score}"
+      puts "#{player.to_s.ljust(max_name_length)} : #{score}"
+    end
     
-    case
-      when @scores.values.inject(:+) == 0
-        puts "\n\t~ Draw ~"
+    unless @scores.values.inject(:+) == 0
+      case
+      when @scores[@scores.keys[0]] == @scores[@scores.keys[1]]
+        puts "\n\t~ Draw ~\n\n"
       when @scores[@scores.keys[0]] > @scores[@scores.keys[1]]
-        puts "#{@scores.values[0]} is the WINNER!!"
-      when @scores[@scores.keys[0]] > @scores[@scores.keys[1]]
-        puts "#{@scores.values[1]} is the WINNER!!"
+        puts "\n\t#{@scores.keys[0]} is winning!\n\n"
+      when @scores[@scores.keys[0]] < @scores[@scores.keys[1]]
+        puts "\n\t#{@scores.keys[1]} is winning!\n\n"
+      end
     end
   end
-  
+
   def get_players
     begin
       puts "How many players?"
       print "\t>>"
       response = gets.chomp
-      
+
       unless ["0", "1", "2"].include? response
         raise StandardError
       end
-      
+
     rescue StandardError
       puts "This game supports 0 to 2 players"
       retry
@@ -78,49 +85,147 @@ class Game
       @options[:mode] = response.to_i
     end
   end
-  
-  def get_char_pref
-    unless @options[:mode] == 0
-      puts "What character set should we use?"
-      print "\t>>"
-      response = get.chomp.to_s[0, 2]
-    else
-      response = "XO"
-      
-    @options[:char] = response.length < 2 ? "XO" : [response[0], response[1]]
-  end
-  
+
   def get_starter
-    unless @options[:mode] == 0
+    case @options[:mode]
+    when 0
+      response = "y"
+    when 1
       puts "Do you want to go first?"
       print "\t>>"
-      response = gets.chomp.downcase.gsub(' ', '')
+      response = gets.chomp.downcase.gsub(" ", "")
+    when 2
+      puts "Which player is going first?"
+      print "\t>>"
+      response = gets.chomp.scan(/\d/)
+
+      response = response.empty? ? ["1"] : response
+      response = response[0] == "1" ? "y" : "n"
     else
-      response = 0
-      
+      puts "Error: bad value for mode:- #{@options[:mode]}!"
+    end
+
     @affirmative.include? response ? @options[:turn] = 0 : @options[:turn] = 1
   end
-  
+
   def set_players
+    puts "Setting players"
     case @options[:mode]
-    when 0 then @scores[:Robot1]  = @scores [:Robot2]  = 0
-    when 1 then @scored[:Player1] = @scores [:Robot1]  = 0
-    when 2 then @scored[:Player1] = @scores [:Player2] = 0
+    when 0 then @scores[:Robot2]  = @scores [:Robot1]  = 0
+    when 1 then @scores[:Robot1] = @scores [:Player1]  = 0
+    when 2 then @scores[:Player2] = @scores [:Player1] = 0
     else puts "Error: get players before setting them"
+    end
+    puts 
   end
-  
+
   def finish
-    @scores.values.inject(:+) != 0 ? get_scoreboard() : nil
-    puts "\nGoodbye"
+    puts "Goodbye"
     exit
   end
+  
+  def test
+    puts "Hi there"
+    print "How's it going?"
+    sleep 2
+    print "\r" + ("\e[A\e[K")
+    print "It's me again"
+  end
+  
+  private :get_players, :get_starter, :set_players, :go_to_board
+  attr_reader :scores
+  attr_accessor :options
 end
 
 class Board
+  def initialize(game)
+    @game = game
+    @status = Hash.new
+
+    @game.scores.keys.each { |player| @status[player] = [] }
+    puts "#{@status}"
+  end
+
   def play
+    view = build
+    create(view)
+    state = false
+
+    while true
+      state = check_status()
+      if state.nil?
+        while true
+          current_player = @status.keys[@game.options[:turn]]
+          puts "#{current_player} next move~"
+          print "\t>>"
+          move = gets.chomp.to_i
+          puts move
+          puts @status
+          if ((1..9).include? move) && !(@status.values.flatten.include? move)
+            @status[current_player] << move
+            @game.options[:turn]  ^= 1
+            break
+          else
+            puts "Invalid move #{move}!"
+          end
+        end
+
+        view = build
+        create(view)
+      else
+        break
+      end
+    end
+
+    state
   end
+
   def build
+    users_char = Hash.new
+    [0, 1].each { |i| users_char[@game.scores.keys[i]] = @game.options[:charset][i] }
+
+    moves = Array.new(9)
+
+    @status.each do |player, player_moves|
+      (1..9).each do |i|
+        moves[i-1] = (player_moves.include? i) ? users_char[player] : moves[i-1]
+      end
+    end
+
+    moves.map! { |x| x ? x : " " }
+
+    moves
   end
+
+  def create(moves)
+    top    = "   |   |   "
+    bottom = "___|___|___"
+
+    puts top
+    puts " #{moves[6]} | #{moves[7]} | #{moves[8]} "
+    puts bottom
+    puts top
+    puts " #{moves[3]} | #{moves[4]} | #{moves[5]} "
+    puts bottom
+    puts top
+    puts " #{moves[0]} | #{moves[1]} | #{moves[2]} "
+    puts top
+  end
+
   def turn
+  end
+
+  def check_status
+    winner = nil
+    
+    @status.keys.each do |player|
+      moves = @status[player].sort
+      moves = moves.product(moves).product(moves).map { |i| i.flatten.sort }
+      moves.each { |move| winner = player if WINNING_GAMES.include? move }
+      
+      break unless winner.nil?
+    end
+
+    winner
   end
 end
